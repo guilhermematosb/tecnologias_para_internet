1d22944a466cca984df2d51aa7e7f40378636ebe
Guilherme <guilhermematosb@gmail.com>
2024-03-20 19:07:23 -0300

    Complemento do exemplo da aPI com JPA

--- springjpa/src/main/java/com/senac/springjpa/controllers/TaskController.java
+++ springjpa/src/main/java/com/senac/springjpa/controllers/TaskController.java
@@ -1,35 +1,62 @@
 package com.senac.springjpa.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.senac.springjpa.entity.TaskEntity;
 import com.senac.springjpa.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
-    private TaskRepository repository;
+    private TaskRepository taskRepository;
 
     @GetMapping
     public ResponseEntity<List<TaskEntity>> getAllTasks() {
-        return ResponseEntity.ok(this.repository.findAll());
+        List<TaskEntity> tasks = taskRepository.findAll();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskEntity> getTaskById(@PathVariable Long id) {
+        Optional<TaskEntity> optionalTask = taskRepository.findById(id);
+        return optionalTask.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public TaskEntity createTask(@RequestBody TaskEntity TaskEntity) {
-        
-        return this.repository.save(TaskEntity);
+    public ResponseEntity<TaskEntity> createTask(@RequestBody TaskEntity task) {
+        TaskEntity savedTask = taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
-    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskEntity> updateTask(@PathVariable Long id, @RequestBody TaskEntity updatedTask) {
+        Optional<TaskEntity> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            TaskEntity task = optionalTask.get();
+            task.setTitle(updatedTask.getTitle());
+            task.setDescription(updatedTask.getDescription());
+            task.setCompletionDate(updatedTask.getCompletionDate());
+            TaskEntity savedTask = taskRepository.save(task);
+            return ResponseEntity.ok(savedTask);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- springjpa/src/main/java/com/senac/springjpa/repository/TaskRepository.java
+++ springjpa/src/main/java/com/senac/springjpa/repository/TaskRepository.java
@@ -1,7 +1,5 @@
 package com.senac.springjpa.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +9,6 @@ import com.senac.springjpa.entity.TaskEntity;
  * TaskRepository
  */
 @Repository
-public interface TaskRepository extends JpaRepository<TaskEntity, UUID>{
+public interface TaskRepository extends JpaRepository<TaskEntity, Long>{
     
 }
