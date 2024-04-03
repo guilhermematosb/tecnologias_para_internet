04e1e34dfa3eb842521626ab4bf25cd717994939
Guilherme <guilhermematosb@gmail.com>
2024-04-03 18:28:17 -0300

    organizando pom.xml e add dependencia para validação

--- cursos/src/main/java/com/senac/cursos/entity/Curso.java
+++ cursos/src/main/java/com/senac/cursos/entity/Curso.java
@@ -4,9 +4,11 @@ import java.util.Date;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,6 +19,7 @@ import lombok.Data;
 // Usando Lombok para gerar automaticamente getters, setters, toString, hashCode e equals
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Curso {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
