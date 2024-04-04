1822f96c6c6845c974460b4e244bbba6808864fb
Guilherme <guilhermematosb@gmail.com>
2024-04-04 20:51:17 -0300

    melhorando as mensagens de erro

--- cursos/src/main/java/com/senac/cursos/entity/Usuario.java
+++ cursos/src/main/java/com/senac/cursos/entity/Usuario.java
@@ -16,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -29,13 +30,15 @@ public class Usuario {
     private Long id;
     private String name;
 
-    @Pattern(regexp = "^[^\\s]+$", message = "O campo [username] não deve conter espaço")
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
 
     @Length(min = 6, max = 100)
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
     private String password;
     private String description;
     private String curriculum;
---
+++ cursos/src/main/java/com/senac/cursos/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package com.senac.cursos.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+  private String message;
+  private String field;
+}
