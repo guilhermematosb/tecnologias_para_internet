39c96e1f0c0fd5a8e1ab760d3931be83da391ec5
Guilherme <guilhermematosb@gmail.com>
2024-05-15 18:31:01 -0300

    Add files via upload

---
+++ bimestre_02/introducao_javascript/js/precedencia.js
@@ -0,0 +1,27 @@
+// Parênteses (): 
+// Eles têm a precedência mais alta e são usados para 
+// forçar a ordem de avaliação em expressões.
+// resultado = 2 + 3 * 4
+
+// Exponenciação **: 
+// Este operador calcula uma base elevada a uma potência.
+// resultado = 2 ** 3 * 4
+
+// Multiplicação *, Divisão /, Divisão inteira //, e Módulo %: 
+// Estes operadores têm a mesma precedência e são avaliados 
+// da esquerda para a direita.
+// resultado = 10 / 2 * 3
+
+// Adição + e Subtração -: 
+// Assim como a categoria anterior, estes operadores têm a mesma 
+// precedência e são avaliados da esquerda para a direita.
+resultado = 10 - 5 + 3
+
+// Operadores de comparação (<, <=, >, >=, ==, !=) e in, not in, is, is not: 
+// Eles são usados para comparar valores e têm menor precedência que os 
+// operadores aritméticos.
+resultado = 10 <= 5 + 3 * 2
+console.log(resultado)
+
+
+
